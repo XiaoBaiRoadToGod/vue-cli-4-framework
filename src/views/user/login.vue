e24aa1bbf6afaa7861cb903cf48af65c8324a433
zjVirus <285857527@qq.com>
2020-03-18 13:56:35 +0800

    保存时自动使用eslint效验规则

--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -10,34 +10,34 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
-  export default {
-    data() {
-      return {
+export default {
+  data() {
+    return {
 
-      }
-    },
-    computed: {
-      ...mapGetters({
-        token: 'user/token'
+    }
+  },
+  computed: {
+    ...mapGetters({
+      token: 'user/token'
+    })
+  },
+  mounted() {
+
+  },
+  methods: {
+    ...mapActions({
+      login: 'user/login'
+    }),
+    clickLogin() {
+      this.login(123).then(res => {
+        console.log(res)
+        if (res.token) {
+          alert('已登录')
+        }
       })
-    },
-    mounted() {
-
-    },
-    methods: {
-      ...mapActions({
-        login: 'user/login'
-      }),
-      clickLogin() {
-        this.login(123).then(res => {
-          console.log(res)
-          if(res.token) {
-            alert('已登录')
-          }
-        })
-      }
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
